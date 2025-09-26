41e32860dc716516a773429c031aaead99b14088
Landi <joaomarcospsnbr@gmail.com>
2025-09-26 04:48:46 -0300

    feat: add scheduling logs

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('php artisan queue:retry all')->everyFiveMinutes();
+Schedule::command('queue:retry all')->everyFiveMinutes()->withoutOverlapping()->onOneServer();
